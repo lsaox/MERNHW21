b40613de386e29afdfa9ade63c0a1613604f6b74
unknown <el_word9@yahoo.com>
2024-03-17 15:07:08 -0500

    Importing apollo in appjsx

--- Develop/client/src/App.jsx
+++ Develop/client/src/App.jsx
@@ -1,7 +1,33 @@
 import './App.css';
 import { Outlet } from 'react-router-dom';
+//Added from apollo client
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
 
 import Navbar from './components/Navbar';
+//Make graphql endpoint
+const httpLink = createHttpLink({ uri: '/graphql' });
+
+
+//Construct request middleware thatll connect the JWT token
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = window.localStorage.getItem('token');
+  // return the headers to the context 
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
 
 function App() {
   return (
