b3f1551fef2adb3e7761d8439defd0baecf0b60c
unknown <el_word9@yahoo.com>
2024-03-17 14:53:23 -0500

    added mutations

---
+++ Develop/client/src/utils/mutations.js
@@ -0,0 +1,67 @@
+
+
+import { gql } from '@apollo/client';
+//added mutations.js which as   * `LOGIN_USER` will execute the `loginUser` mutation set up using Apollo Server.
+//`ADD_USER` will execute the `addUser` mutation.
+//`SAVE_BOOK` will execute the `saveBook` mutation.
+// `REMOVE_BOOK` will execute the `removeBook` mutation.
+
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($bookData: BookInput!) {
+    saveBook(bookData: $bookData) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        image
+        description
+        title
+        link
+      }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: ID!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        authors
+        image
+        description
+        title
+        link
+      }
+    }
+  }
+`;
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
